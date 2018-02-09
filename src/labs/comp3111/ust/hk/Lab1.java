97002667b808e6c40e97e1e94feb438c122e4c11
wonyoung1026 <wonyoung1026@hotmail.com>
2018-02-09 13:39:49 +0800

    Initial project setup

---
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -0,0 +1,10 @@
+package labs.comp3111.ust.hk;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Lab1: Introduction to Git and Github");
+	}
+
+}
