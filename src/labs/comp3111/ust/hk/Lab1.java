162035739ed09c202eb3d98d08d518a6913125f3
wonyoung1026 <wonyoung1026@hotmail.com>
2018-02-09 13:40:34 +0800

    Added a congratulation message

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -5,6 +5,7 @@ public class Lab1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Lab1: Introduction to Git and Github");
+		System.out.println("Congratulation! You learned the Git Basics");
 	}
 
 }
